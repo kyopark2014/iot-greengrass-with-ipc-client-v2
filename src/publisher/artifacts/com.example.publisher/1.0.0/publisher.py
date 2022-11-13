7ba7b0fc09e56bc9cf1ab7223af4a3588429f07b
ksdyb.park <ksdyb@amazon.com>
2022-11-13 15:11:04 +0900

    update

--- src/publisher/artifacts/com.example.publisher/1.0.0/publisher.py
+++ src/publisher/artifacts/com.example.publisher/1.0.0/publisher.py
@@ -1,5 +1,6 @@
 import sys
 import traceback
+import time
 from awsiot.greengrasscoreipc.clientv2 import GreengrassCoreIPCClientV2
 from awsiot.greengrasscoreipc.model import (
     PublishMessage,
@@ -12,14 +13,20 @@ def main():
 
     try:
         ipc_client = GreengrassCoreIPCClientV2()
-        binary_message = BinaryMessage(message=bytes(message, 'utf-8'))
-        publish_message = PublishMessage(binary_message=binary_message)
-        ipc_client.publish_to_topic(topic=topic, publish_message=publish_message)
+
+        while True: 
+            publish_binary_message_to_topic(ipc_client, topic, message)
+            time.sleep(5)
 
     except Exception:
         print('Exception occurred', file=sys.stderr)
         traceback.print_exc()
         exit(1)
+
+def publish_binary_message_to_topic(ipc_client, topic, message):
+    binary_message = BinaryMessage(message=bytes(message, 'utf-8'))
+    publish_message = PublishMessage(binary_message=binary_message)
+    ipc_client.publish_to_topic(topic=topic, publish_message=publish_message)
 
 if __name__ == '__main__':
     main()        
